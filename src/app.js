d79661cc93aa2e127ca8d53e2ed160fe392902ba
Rahul Dhakad <86297971+githubRahuld@users.noreply.github.com>
2025-01-10 16:51:26 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -4,12 +4,19 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 
-app.use(
-    cors({
-        origin: process.env.CORS_ORIGIN,
-        credentials: true,
-    })
-);
+// cors configuration
+const corsOptions = {
+  origin:process.env.CORS_ORIGIN,
+  credentials: true, 
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"], 
+  allowedHeaders: ["Content-Type", "Authorization"], 
+};
+
+// Apply CORS to all routes
+app.use(cors(corsOptions));
+
+// Ensure preflight (OPTIONS) requests also use the same CORS settings
+app.options("*", cors(corsOptions));
 
 app.use(express.json({ limit: "16kb" })); //to allow json data
 
